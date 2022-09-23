560b87cb871d596a2788606044fedacdd8ae0d04
Joshua G Monger <joshmonger@gmail.com>
2022-09-23 10:40:37 -0600

    Added song

--- c/Songlesson1/Music.cpp
+++ c/Songlesson1/Music.cpp
@@ -2,5 +2,9 @@
 #include "Song.hpp"
 
 int main() {
+    
+  Song electric_relaxation;
+  electric_relaxation.add_title("Electric Relaxation");
+  std::cout << electric_relaxation.get_title() << "\n";
 
 }
