50fd61bd884b8107670d69fda48387de0e99668a
Joshua G Monger <joshmonger@gmail.com>
2022-12-20 11:44:56 -0700

    Testing again

--- c/Songlesson1/Song.cpp
+++
@@ -1,9 +0,0 @@
-#include "Song.hpp"
-
-// add Song method definitions here:
-void Song::add_title(std::string new_title){
-  title = new_title;
-}
-std::string Song::get_title(){
-  return title;
-}
